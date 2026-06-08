0f00155ad0e4ebacf0d837db066f4739c6e5c380
macquin1800 <mac.quin.1800@gmail.com>
2026-06-08 23:57:07 +0100

    chore: Version fonctionnel de solvia admin

--- backend/src/modules/auth/policy/auth-token-policy.service.ts
+++ backend/src/modules/auth/policy/auth-token-policy.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import ms from "ms";
+import * as ms from "ms";
 
 @Injectable()
 export class AuthTokenPolicyService {
